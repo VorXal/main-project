a0c10e5d7f7a1a0e47b631672591c8d2bd6af50f
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-05-26 22:23:07 +0300

    Update List.py
    Мельком набросал решение второй задачи, но чую придется ее переделать. Совсем.

--- Python/Lesson/List.py
+++ Python/Lesson/List.py
@@ -11,3 +11,20 @@ def sum_items(array):
 
 some_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(sum_items(some_list))
+
+
+# 2) Write a Python program to remove duplicates from a list.
+print("Python program to remove duplicates from a list")
+
+
+def delete_duplicate(array):
+    for i in array:
+        print("i = ", i)
+        for j in array[1::]:
+            print("j = ", j)
+            if array[i] == array[j]:
+                print("WHATS WRONG WITH YOU?")
+
+
+duplicate_list = [7, 1, 1, 3, 4, 4, 6, 7, 8, 4]
+print(delete_duplicate(duplicate_list))
