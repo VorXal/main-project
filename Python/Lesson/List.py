fca004daaf3874b738a0e65d3c9d7615dc7df34a
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-05-29 11:19:08 +0300

    Update List.py
    Переделал второе задание, добавил третье

--- Python/Lesson/List.py
+++ Python/Lesson/List.py
@@ -18,13 +18,28 @@ print("Python program to remove duplicates from a list")
 
 
 def delete_duplicate(array):
-    for i in array:
-        print("i = ", i)
-        for j in array[1::]:
-            print("j = ", j)
-            if array[i] == array[j]:
-                print("WHATS WRONG WITH YOU?")
+    array = set(array)
+    array = list(array)
+    return array
 
 
 duplicate_list = [7, 1, 1, 3, 4, 4, 6, 7, 8, 4]
 print(delete_duplicate(duplicate_list))
+
+
+# 3) Write a Python program to append a list to the second list.
+print("Python program to append a list to the second list")
+
+
+def list_append(first_list, second_list):
+    new_list = []
+    for i in first_list:
+        new_list.append(i)
+    for i in second_list:
+        new_list.append(i)
+    return new_list
+
+
+some_list = [1, 5, 3, 6]
+yet_some_list = [1, 5, 3, 2, 5, 6]
+print(list_append(some_list, yet_some_list))
