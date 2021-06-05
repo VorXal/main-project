71895f6a9b958f7d99cb277818ac58a7391bce1c
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-05 11:00:48 +0300

    Исправил некоторые косяки

--- Python/Lesson/Set.py
+++ Python/Lesson/Set.py
@@ -2,13 +2,13 @@
 print("Python program to iterate over sets (and multiply every element on 2)")
 
 
-def set_multiplication(incoming_set):
+def multiplication_set(incoming_set):
     return {i*2 for i in incoming_set}
 
 
 some_set = {1, 3, 5, 7, 1, 95, 4}
 print("Data: ", some_set)
-print("Result: ", set_multiplication(some_set), "\n")
+print("Result: ", multiplication_set(some_set), "\n")
 
 
 # 2) Write a Python program to add member(s) in a set
@@ -29,29 +29,29 @@ print("Result: ", add_set_member(some_set, some_date), "\n")
 print("Python program to create an intersection of sets")
 
 
-def set_intersection(incoming_set, yet_incoming_set):
+def intersection_set(incoming_set, yet_incoming_set):
     return incoming_set.intersection(yet_incoming_set)
 
 
 some_set = {1, 3, 5}
 some_date = {5, 1, 234}
 print("Data: ", some_set, "and", some_date)
-print("Result: ", set_intersection(some_set, some_date), "\n")
+print("Result: ", intersection_set(some_set, some_date), "\n")
 
 
 # 4) Write a Python program to create a shallow copy of sets
 print("Python program to create a shallow copy of sets")
+import copy
 
-
-def set_intersection(incoming_set):
-    set_copy = incoming_set
+def shallow_copy(incoming_set):
+    set_copy = incoming_set.copy()
     set_copy.add(44)
     return set_copy
 
 
 some_set = {1, 3, 5}
 print("Data: ", some_set)
-print("Result: ", set_intersection(some_set))
+print("Result: ", shallow_copy(some_set))
 print("Data: ", some_set)
 
 
