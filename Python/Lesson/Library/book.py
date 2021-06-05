6b7e7d3ebb5e3e6b1441d6423edde153a9fbac66
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-06 00:33:05 +0300

    Добавил модуль book
    Код, касающийся самих книг

---
+++ Python/Lesson/Library/book.py
@@ -0,0 +1,38 @@
+from author import Author
+
+
+class Book:
+
+    # Конструктор для книг
+    def __init__(self, name, author, price, edition):
+        self.__name = name
+        if type(author) == Author:
+            self.__author = author
+        else:
+            print("Author - не является объектом!")
+        self.__price = price
+        self.__edition = edition
+
+    def get_name(self):
+        return self.__name
+
+    def get_author(self):
+        return self.__author
+
+    def get_price(self):
+        return self.__price
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_edition(self):
+        return self.__edition
+
+    def set_edition(self, edition):
+        self.__edition = edition
+
+    def output_info(self):
+        print("Книга", self.__name)
+        print("Автор", self.__author)
+        print("Цена", self.__price)
+        print("Редакция", self.__edition)
