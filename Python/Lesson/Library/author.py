6c681e75fd0021a2dd73dc43b2af041801f8aa78
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-06 12:30:32 +0300

    Добавил __str__

--- Python/Lesson/Library/author.py
+++ Python/Lesson/Library/author.py
@@ -1,11 +1,15 @@
 class Author:
 
     # Конструктор объекта автор
-    def __init__(self, name, email, gender, age):
+    def __init__(self, author_id, name, email, gender, age):
+        self.__author_id = author_id
         self.__name = name
         self.__email = email
         self.__gender = gender
         self.__age = age
+
+    def __str__(self):
+        return "Имя: {} Возраст: {} Пол: {} Email: {}".format(self.__name, self.__age, self.__gender, self.email)
 
     # Получаем имя автора
     def get_name(self):
