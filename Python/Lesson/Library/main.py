a46f893cc324dd5b7c0010a2f1e1fed356c04d0e
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-06 12:30:04 +0300

    Накрутил создание авторов

--- Python/Lesson/Library/main.py
+++ Python/Lesson/Library/main.py
@@ -1,15 +1,46 @@
 import author
+import book
 
+print("Добавить автора? (Y/N)")
+open_editor = input()
+if open_editor == "Y":
+    flag = True
+else:
+    flag = False
 
-# Создаем в конструкторе автора - Tom
-Tom = author.Author("Tom", "Tom@gmail.com", "Male", 35)
-Tom.output_info()
+author_id = 1
+author_list = list()
 
+while flag:
+    print("Добавление автора. Введите имя:")
+    name = input()
+    print("Введите возраст:")
+    age = int(input())
+    print("Введите пол (Male/Female):")
+    gender = input()
+    print("Введите e-mail автора:")
+    email = input()
 
-# Меняем e-mail Toma
-print("Введите свой e-mail")
-Tom.email = input()
+    # Занесение в бд? Или в лист?
+    new_author = author.Author(author_id, name, email, gender, age)
+    author_id += 1
+    author_list.append(new_author)
 
+    print("Автор:", new_author, "добавлен")
+    print("Добавить еще автора? (Y/N)")
+    author_editor_exit = input()
+    if author_editor_exit == "N":
+        flag = False
 
-# Выводим информацию об авторе - Tom
-Tom.output_info()
+# Создаем доп авторов в конструкторе
+Nattzy = author.Author(2, "Лимонов Роман", "new.nattzy@gmail.com", "Male", 24)
+Vorxal = author.Author(3, "Воронков Алексей", "vorxal@gmail.com", "Male", 21)
+
+author_list.append(Nattzy)
+author_list.append(Vorxal)
+
+print(author_list)
+
+# Создаем в конструкторе книги
+Nolan = book.Book("Нолан", author_list, 400, "Первая")
+Nolan.output_info()
