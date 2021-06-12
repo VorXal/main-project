b41baea452209162657647c8cec9f5b9384c21cc
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-12 17:35:29 +0300

    Переделываем в принципе все ._.
    Не скажу что стало намного лучше, но...

--- Python/Work/Bot/main.py
+++ Python/Work/Bot/main.py
@@ -1,46 +1,34 @@
 import telebot
 import config
 import ticket
-
+import keyboard
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    bot.send_message(
-        message.chat.id,
-        'Обработка событий'
-    )
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(telebot.types.InlineKeyboardButton('Work?', callback_data='print'))
-    keyboard.add(telebot.types.InlineKeyboardButton('Заявка', callback_data='ticket'))
-    bot.send_message(
-        message.chat.id,
-        'Отправить запрос на сервер',
-        reply_markup=keyboard
-    )
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!"
+                     .format(message.from_user, bot.get_me()), reply_markup=keyboard.ticket_markup)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    print(message.text)
-    if type(message.text) == str:
-        theme_message = message.text
-        text_message = message.text*10
-        output = ticket.Ticket(theme_message, text_message)
-        bot.send_message(message.from_user.id, output.create_ticket())
+    if message.text == "Создать заявку":
+        bot.send_message(message.from_user.id, "Введите сообщение, пример:\n\nНе работает VPN"
+                                               "\n*Подробное описание проблемы*"
+                                               "\n\nДля отмены нажмите - Отмена", reply_markup=keyboard.close_markup)
+        bot.register_next_step_handler(message, open_ticket)
+    elif message.text == "Создать отчет":
+        bot.send_message(message.from_user.id, "Функция пока недоступна")
+    else:
+        bot.send_message(message.from_user.id, "Команда не распознана")
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def iq_callback(query):
-    data = query.data
-    if data.startswith('print'):
-        print("Button activated!")
-        bot.send_message(query.from_user.id, "WORK!")
-    elif data.startswith('ticket'):
-        print("New ticket!")
-        bot.send_message(query.from_user.id, "Введите тему письма")
+def open_ticket(message):
+    output = ticket.Ticket(message.text)
+    bot.send_message(message.from_user.id, output.create_ticket())
+    bot.send_message(message.from_user.id, "Чем я еще могу помочь?", reply_markup=keyboard.ticket_markup)
 
 
 bot.polling(none_stop=True)
