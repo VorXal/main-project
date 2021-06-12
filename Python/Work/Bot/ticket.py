3f62435fc6ec7fe2244c5bb3cba7d2fcc530bede
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-12 17:36:58 +0300

    Добавил функционала
    Пока немного на костылях, каюсь, но в будущем, думаю исправлю

--- Python/Work/Bot/ticket.py
+++ Python/Work/Bot/ticket.py
@@ -1,8 +1,31 @@
+import re
+
+
 class Ticket:
 
-    def __init__(self, theme, message):
-        self.__theme = theme
-        self.__message = message
+    # Обрезаем сообщение от пользователя
+    def __init__(self, message):
+        if message == "Отмена":
+            self.__theme = "Отмена"
+        else:
+            self.__theme = re.match('(.*)', message).group(0)
+            self.__message = message.replace(self.__theme, "", 1)
 
     def create_ticket(self):
-        return "Тема письма: "+self.__theme+"\n\nТело письма: "+self.__message
+        error = ""
+        alert = False
+        if self.__theme == "Отмена":
+            return "Создание заявки отменено"
+        else:
+            if len(self.__message) < 2:
+                error += "Не указана тема сообщения\n"
+                alert = True
+            if len(self.__message) < 20:
+                error += "Слишком короткое сообщение\n"
+                alert = True
+
+            if alert == bool(1):
+                return error
+            else:
+                # Отправка письма на почту и возвращение ответа
+                return "Тикет создан!"
