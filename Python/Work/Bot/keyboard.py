afbeedde3dfd70805107d816559860787fa7c45f
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-13 11:29:17 +0300

    Дополнил клавиатуры
    Вот теперь точно быдлокод

--- Python/Work/Bot/keyboard.py
+++ Python/Work/Bot/keyboard.py
@@ -2,14 +2,27 @@ from telebot import types
 
 
 # Создаем клавиатуры
-ticket_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+qa_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 close_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
 # Создаем кнопки
 ticket_button = types.KeyboardButton("Создать заявку")
 report_button = types.KeyboardButton("Создать отчет")
+qa_button = types.KeyboardButton("Оценить работу отдела")
+qa_one_button = types.KeyboardButton("1. Серьезные проблемы")
+qa_two_button = types.KeyboardButton("2. Довольно плохо")
+qa_three_button = types.KeyboardButton("3. Удовлетворительно")
+qa_four_button = types.KeyboardButton("4. Достаточно хорошо")
+qa_five_button = types.KeyboardButton("5. Все отлично!")
 close_button = types.KeyboardButton("Отмена")
 
 # Привязываем кнопки к клавиатуре
-ticket_markup.add(ticket_button, report_button)
+menu_markup.add(ticket_button, report_button)
+menu_markup.add(qa_button)
+qa_markup.add(qa_five_button)
+qa_markup.add(qa_four_button)
+qa_markup.add(qa_three_button)
+qa_markup.add(qa_two_button)
+qa_markup.add(qa_one_button)
 close_markup.add(close_button)
