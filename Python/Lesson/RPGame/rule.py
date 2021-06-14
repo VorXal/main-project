2b6db9a7684ceda7c9eb3fc77340d885a785860f
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-14 12:03:42 +0300

    Delete rule.py

--- Python/Lesson/RPGame/rule.py
+++
@@ -1,9 +0,0 @@
-class Rule:
-
-    def __init__(self, score, death):
-        self.__score = score
-        self.__death = death
-
-    def game_over(self):
-        if self.__death is True:
-            return "Game is over! Your score:"+self.__score
