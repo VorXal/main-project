5d39987f3098db472996fbe46610017e0957bade
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-06-14 12:04:18 +0300

    Update main.py

--- Python/Lesson/RPGame/main.py
+++ Python/Lesson/RPGame/main.py
@@ -1,9 +1,14 @@
+from rulebook import Rule
 from player import Player
 from enemy import Enemy
+from fight import Fight
 
 
 if __name__ == "__main__":
-    gamer = Player("Бальтазар", 1, 1000, 1000, 200, 100)
+    game = Rule()
+    gamer = Player("Бальтазар")
     while True:
         monster = Enemy(gamer.get_level())
-        print(monster.get_info())
+        print("\nНа арене появляется", monster.get_name(), "!")
+        battle = Fight(monster.get_level())
+        print(battle.arena(gamer, monster, game))
