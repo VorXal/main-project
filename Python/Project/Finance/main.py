f596987fba3fee0793ef856ffb8556287277f29c
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-07-25 00:01:11 +0300

    Create main.py

---
+++ Python/Project/Finance/main.py
@@ -0,0 +1,26 @@
+from user import User
+from pymongo import MongoClient
+from interaction import Interaction
+
+
+if __name__ == "__main__":
+    admin = User("Admin", "DeGenerate12Pas")
+    client = MongoClient(admin.get_connect())
+    db = client.fincon
+    spending = db.spending
+
+    while True:
+        print("Введите действие")
+        action = input()
+        if action == "Вывести все":
+            print(Interaction.get_all(spending))
+        elif action == "Добавить":
+            print("Введите название")
+            name = input()
+            print("Введите тип")
+            type = input()
+            print("Введите цену")
+            cost = int(input())
+            Interaction.insert(spending, name, type, cost)
+        else:
+            print("Команда не распознана")
