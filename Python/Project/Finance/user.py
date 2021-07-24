145bec87aac4a6f67d0b697b96db965c0d0d30a0
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2021-07-25 00:04:29 +0300

    Update user.py

--- Python/Project/Finance/user.py
+++ Python/Project/Finance/user.py
@@ -3,8 +3,7 @@ class User:
     def __init__(self, user, password):
         self.__user = user
         self.__password = password
-        self.__connect = "mongodb+srv://"+user+":"+password+"" \
-          "@financecontrol.slgh6.mongodb.net/FinanceControl?retryWrites=true&w=majority"
+        self.__connect = "your"+user+":"+password+"path"
 
     def get_connect(self):
         return self.__connect
